31706c4e76bcb949bc9502ae5c12ba4c3d259edf
Klayten Richmond <klaytenrichmond@gmail.com>
2018-03-11 12:21:54 -0700

    initialA

---
+++ apps/likes/models.py
@@ -0,0 +1,21 @@
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+
+class Users(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)
+
+
+class Book(models.Model):
+    name = models.CharField(max_length=255)
+    desc = models.TextField(1000)
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)
+    uploaded_books = models.ForeignKey(Users, related_name="uploader")
+    liked_books = models.ManyToManyField(Users, related_name="liked_users")
